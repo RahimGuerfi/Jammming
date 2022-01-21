5f783e753b6d2c0e58b35ddd60d4b96474f8d222
Guerfi Abderrahim <89612748+RahimGuerfi@users.noreply.github.com>
2022-01-21 15:29:36 +0100

    Fixed a mistake inside App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      blocking: true,
+      blocking: false,
       searchResults: [],
       playlistName: "Playlist Name",
       playlistTracks: [],
